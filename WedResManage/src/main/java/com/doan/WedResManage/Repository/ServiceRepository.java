2df2cad47b7cadb3eaa903f2210a020c5a6cc840
giahuyoke <giahuyoke01@gmail.com>
2022-10-14 16:30:38 +0700

    Update OrderRequest.java

--- WedResManage/src/main/java/com/doan/WedResManage/Repository/ServiceRepository.java
+++ WedResManage/src/main/java/com/doan/WedResManage/Repository/ServiceRepository.java
@@ -1,9 +1,15 @@
 package com.doan.WedResManage.Repository;
 
+import com.doan.WedResManage.pojo.ListService;
 import com.doan.WedResManage.pojo.Service;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface ServiceRepository extends JpaRepository<Service, Integer>, JpaSpecificationExecutor<Service> {
+import java.util.List;
 
+public interface ServiceRepository extends JpaRepository<Service, Integer>, JpaSpecificationExecutor<Service> {
+    Page<Service> searchServiceByNameContains(String s, Pageable pageable);
+    List<Service> findAllById(int id);
 }
