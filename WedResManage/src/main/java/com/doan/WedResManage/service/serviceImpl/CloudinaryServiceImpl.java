01bc11bf2f7713f7e12c8435a72bdb45f9089128
Gia Huy <giahuyoke01@gmail.com>
2023-04-01 18:31:53 +0700

    Hello

--- WedResManage/src/main/java/com/doan/WedResManage/service/serviceImpl/CloudinaryServiceImpl.java
+++ WedResManage/src/main/java/com/doan/WedResManage/service/serviceImpl/CloudinaryServiceImpl.java
@@ -7,14 +7,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
+
 @Service
 public class CloudinaryServiceImpl implements CloudinaryService {
     @Override
     public String uploadImg(MultipartFile file, Cloudinary cloudinary) {
         try {
-            cloudinary.uploader().upload(file.getBytes(),
+            Map uploadResult = cloudinary.uploader().upload(file.getBytes(),
                     ObjectUtils.asMap("resource_type", "auto"));
-            return "true";
+            String imageUrl = (String) uploadResult.get("url");
+            return imageUrl;
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
             return "false";
